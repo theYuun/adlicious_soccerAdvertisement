6e4f7116760aa2decaf927be3b7f5c53d1fda520
Theuins Lombard <theunis.lombard@kidsindustries.com>
2022-12-22 22:00:48 +0200

    game

--- javascript.js
+++ javascript.js
@@ -2,8 +2,8 @@ const pageWidth = document.body.clientWidth;
 const pageHeight = document.body.clientHeight;
 
 const canvas = document.getElementById('canvas');
-const width = canvas.width = 300;
-const height = canvas.height = 600;
+const canvasWidth = canvas.width = 300;
+const canvasHeight = canvas.height = 600;
 
 const sizeMultiplier = 0.5;
 
@@ -16,169 +16,395 @@ const mouse = {
 
 const graphics = {
   field: {
-    size: [300, 600],
     graphic: document.getElementById('field'),
+    size: [300, 600],
     position: [0, 0],
   },
   goal: {
-    size: [150, 70],
     graphic: document.getElementById('goal'),
-    position: [75, 5],
-
-    posX: 75,
-    posY: 5,
-    width: 150,
-    height: 70,
+    size: [150, 70],
+    position: [150, 35],
   },
   ball: {
-    size: [100, 100],
     graphic: document.getElementById('ball'),
-    position: [width / 2, height / 2],
+    size: [100, 100],
+    position: [canvasWidth / 2, canvasHeight / 2],
   },
-  character: {
-    size: [180, 240],
+  player: {
     graphic: document.getElementById('character'),
-    position: [10, 10],
+    width: 180,
+    height: 240,
+    size: [180, 240],
   },
+  characters: {
+    graphic: document.getElementById('character'),
+    players: [
+      {
+        position: [100, 60],
+        size: [180, 240],
+        localSizeMultiplier: 1,
+        patrol: [70, 120],
+        speed: [5, 5],
+      },
+      /*
+      {
+        position: [200, 200],
+        size: [180, 240],
+        localSizeMultiplier: 1,
+        patrol: [100, 180],
+        speed: [10, 10],
+      },
+      */
+    ],
+  }
 };
 
-class Character {
-  constructor(graphic, posX, posY, width, height)
-  {
+class Goal{
+  constructor(graphic, position, size, localSize){
+    
     this.graphic = graphic;
-    this.posX = posX;
-    this.posY = posY;
-    this.width = width;
-    this.height = height;
+    this.position = position;
+    this.size = size;
+
+    this.localSizeMultiplier = localSize;
+    this.xOffset = this.size[0] / 2 * this.localSizeMultiplier;
+    this.yOffset = this.size[1] / 2 * this.localSizeMultiplier;
+
+    this.CalculateHitbox();
+    this.DrawGoal();
+  }
+  
+  graphic;
+  position;
+  size;
+
+  localSizeMultiplier;
+  xOffset;
+  yOffset;
+
+  hitBox = {
+    tl: [],
+    tr: [],
+    br: [],
+    bl: [],
+  };
+
+  CalculateHitbox(){
+    this.hitBox =
+    {
+      tl:[
+        this.position[0] - this.xOffset + 5,
+        this.position[1] - this.yOffset],
+      tr:[
+        this.position[0] + this.xOffset - 5,
+        this.position[1] - this.yOffset],
+      br:[
+        this.position[0] + this.xOffset - 5,
+        this.position[1] + this.yOffset - 5],
+      bl:[
+        this.position[0] - this.xOffset + 5,
+        this.position[1] + this.yOffset - 5],
+    };
   }
 
-  graphic = graphics.character.graphic;
-  position = [
-    mouse.x - this.width/2, // horizontal position
-    mouse.y // vertical position
-  ];
-  speed = [
-    5, // horizontal speed
-    5 // vertical speed
-  ];
-  size = [
-    180 * sizeMultiplier * this.localSizeMultiplier, // width
-    240 * sizeMultiplier * this.localSizeMultiplier // height
-  ];
-  localSizeMultiplier = 1;
+  DrawGoal(){
+    
+    ctx.beginPath();
+    ctx.moveTo(this.hitBox.tl[0], this.hitBox.tl[1]);
+    ctx.lineTo(this.hitBox.tr[0], this.hitBox.tr[1]);
+    ctx.lineTo(this.hitBox.br[0], this.hitBox.br[1]);
+    ctx.lineTo(this.hitBox.bl[0], this.hitBox.bl[1]);
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.drawImage(graphics.goal.graphic, this.position[0] - this.xOffset, this.position[1] - this.yOffset, this.size[0], this.size[1]);
+  }
+
+}
+
+class Player{
+  constructor(){
+    this.width = graphics.player.width;
+    this.height = graphics.player.height;
+
+    this.CalculateHitBox();
+  }
+
+  width;
+  height;
+  hitboxHeight = 10;
+  kicking = false;
+  holding = false;
+  kickStrength = -5;
+
+  hitbox = {
+    tl: null,
+    tr: null,
+    br: null,
+    bl: null,
+  }
+
+  CalculateHitBox(){
+    this.hitbox.tl = [mouse.x - this.width/3 * sizeMultiplier, mouse.y - this.hitboxHeight * sizeMultiplier]
+    this.hitbox.tr = [mouse.x + this.width/3 * sizeMultiplier, mouse.y - this.hitboxHeight * sizeMultiplier]
+    this.hitbox.br = [mouse.x + this.width/3 * sizeMultiplier, mouse.y]
+    this.hitbox.bl = [mouse.x - this.width/3 * sizeMultiplier, mouse.y]
+  }
+
+  DrawPlayer() {
+
+    this.CalculateHitBox();
+    // hitbox
+    ctx.beginPath();
+    ctx.moveTo(this.hitbox.tl[0], this.hitbox.tl[1]);
+    ctx.lineTo(this.hitbox.tr[0], this.hitbox.tr[1]);
+    ctx.lineTo(this.hitbox.br[0], this.hitbox.br[1]);
+    ctx.lineTo(this.hitbox.bl[0], this.hitbox.bl[1]);
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(mouse.x, mouse.y, 3, 0, Math.PI * 2);
+    ctx.fillStyle = 'orange';
+    ctx.fill();
+
+    ctx.drawImage(graphics.player.graphic, (mouse.x - this.width/2 * sizeMultiplier), (mouse.y - this.height*sizeMultiplier), this.width * sizeMultiplier, this.height * sizeMultiplier)
+  }
+
+  CatchBall(ballObject){
+    if(!this.kicking)
+    {
+      if(this.hitbox.tr[0] > ballObject.hitBox.tl[0] && this.hitbox.bl[1] > ballObject.hitBox.tl[1] && this.hitbox.tl[0] < ballObject.hitBox.tr[0] && this.hitbox.tl[1] < ballObject.hitBox.br[1])
+      {
+        /*
+        ctx.beginPath();
+        ctx.arc(ballObject.position[0], ballObject.position[1], 3, 0, Math.PI * 2);
+        ctx.fillStyle = 'orange';
+        ctx.fill();
+*/
+        player.holding = true;
+        ballObject.speed = [0, 0];
+      }
+    }
+  }
+
+  HoldBall(ballObject)
+  {
+    if(this.kicking)
+    {
+      this.holding = false;
+    }
+
+    if(this.holding)
+    {
+      ballObject.position = [mouse.x, mouse.y - ballObject.size[1]/2 * sizeMultiplier];
+    }
+  }
+
+  Kick(ballObject){
+    console.log(ballObject.position);
+    ballObject.speed = this.kickStrength;
+    //ballObject.MoveBall();
+    this.holding = false;
+    this.kicking = true;
+    setTimeout(() => this.kicking = false, 100);
+  }
+}
+
+class Character {
+  constructor(graphic, position, size, localSize = 1)
+  {
+    this.graphic = graphic;
+    this.position = position;
+    this.size = size;
+    this.localSizeMultiplier = localSize;
+
+    this.xOffset = this.size[0] / 2 * this.localSizeMultiplier * sizeMultiplier;
+    this.yOffset = this.size[1] / 2 * this.localSizeMultiplier * sizeMultiplier;
+    this.CalculateHitbox();
+  }
+
+  graphic;
+  size;
+  localSizeMultiplier;
+  position;
+  xOffset;
+  yOffset;
+  kickStrength;
   
+  hitBox;
   kicking = false; // a short delay after the player clicks for the ball not to instantly snap to the cursor again
   holding = false; // is the ball stuck to the player character
 
-  posX = mouse.x - this.width/2;
-  posY = mouse.y;
-  width = 180 * sizeMultiplier * this.localSizeMultiplier;
-  height = 240 * sizeMultiplier * this.localSizeMultiplier;
+  CalculateHitbox(){
+    this.hitBox =
+    {
+      tl:[
+        this.position[0] - this.xOffset,
+        this.position[1] - this.yOffset],
+      tr:[
+        this.position[0] + this.xOffset,
+        this.position[1] - this.yOffset],
+      br:[
+        this.position[0] + this.xOffset,
+        this.position[1] + this.yOffset],
+      bl:[
+        this.position[0] - this.xOffset,
+        this.position[1] + this.yOffset],
+    }
+  }
 
   DrawCharacter(){
-    ctx.drawImage(this.graphic, this.posX, this.posY, this.width, this.height)
+    // hitbox
+    ctx.beginPath();
+    ctx.moveTo(this.hitBox.tl[0], this.hitBox.tl[1]);
+    ctx.lineTo(this.hitBox.tr[0], this.hitBox.tr[1]);
+    ctx.lineTo(this.hitBox.br[0], this.hitBox.br[1]);
+    ctx.lineTo(this.hitBox.bl[0], this.hitBox.bl[1]);
+    ctx.closePath();
+    ctx.stroke();
+
+    this.CalculateHitbox();
+    ctx.drawImage(this.graphic, this.hitBox.tl[0], this.hitBox.tl[1], this.size[0] * sizeMultiplier * this.localSizeMultiplier, this.size[1] * sizeMultiplier * this.localSizeMultiplier)
+  }
+
+  PatrolCharacter(){
+
   }
 }
-
+  
 class Ball{
   // Will contain all properties and functions for the ball object
-  DrawBall(){
+  constructor(graphic, pos, size, speed)
+  {
+    this.graphic = graphic; // document.getElementById();
+    this.size = size; // [x, y]
+    this.position = pos; // [x, y]
+    this.speed = speed; // y
+    this.xOffset = this.size[0] / 2 * sizeMultiplier;
+    this.yOffset = this.size[1] / 2 * sizeMultiplier;
+    this.CalculateHitbox();
+  }
 
+  graphic;
+  size;
+  position;
+  speed;
+  xOffset;
+  yOffset;
+  hitBox = {
+    tl: null,
+    tr: null,
+    br: null,
+    bl: null,
+  };
+
+  speedDecelerator = 0.1;
+  scoring = false;
+
+  DrawBall()
+  {
+    this.CalculateHitbox();
+    //console.log(this.hitBox);
+
+    // Draw Hitbox
+    ctx.beginPath();
+    ctx.moveTo(this.hitBox.tl[0], this.hitBox.tl[1]);
+    ctx.lineTo(this.hitBox.tr[0], this.hitBox.tr[1]);
+    ctx.lineTo(this.hitBox.br[0], this.hitBox.br[1]);
+    ctx.lineTo(this.hitBox.bl[0], this.hitBox.bl[1]);
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.drawImage(this.graphic, this.position[0] - this.xOffset, this.position[1] - this.yOffset, this.size[0] * sizeMultiplier, this.size[1] * sizeMultiplier);
   }
 
   MoveBall(){
+    if(!player.holding){
+      this.position[1] += this.speed;
+      if(this.speed > 0)
+      {
+        this.speed -= this.speedDecelerator;
+        if(this.speed < 0)
+          this.speed = 0;
+      }
+      else if(this.speed < 0)
+      {
+        this.speed += this.speedDecelerator;
+        if(this.speed > 0)
+          this.speed = 0;
+      }
+    }
 
   }
 
-  BounceBall(){
-
+  CalculateHitbox(){
+    this.hitBox =
+    {
+      tl:[
+        this.position[0] - this.xOffset,
+        this.position[1] - this.yOffset],
+      tr:[
+        this.position[0] + this.xOffset,
+        this.position[1] - this.yOffset],
+      br:[
+        this.position[0] + this.xOffset,
+        this.position[1] + this.yOffset],
+      bl:[
+        this.position[0] - this.xOffset,
+        this.position[1] + this.yOffset],
+    }
   }
 
-  HoldBall(){
-
+  Score(goalObject, playerObject){
+    if(!playerObject.holding)
+    {
+      // Match boundaries with hitbox to 'detect' boundaries and invert speed for that axis if needed
+      if(this.hitBox.tl[1] < goalObject.hitBox.bl[1] && this.hitBox.tl[0] > goalObject.hitBox.tl[0] && this.hitBox.tr[0] < goalObject.hitBox.tr[0])
+      {
+        console.log('goal');
+        this.speedDecelerator = 0.2;
+        //this.speed *= -1;
+        this.scoring = true;
+        setTimeout(() => this.speed = -10, 500);
+        setTimeout(() => this.ResetBall(), 1000);
+      }
+    }
   }
-
+  
   ResetBall(){
-    
+    this.speed = 0;
+    this.speedDecelerator = 0.1;
+    this.position = [canvasWidth/2, canvasHeight/2]
+    this.scoring = false;
   }
 }
 
-
-
-const opposingCharacters = {
-  graphic: graphics.graphic,
-  players: [
-    {
-      position: [50, 100],
-      size: [180 * sizeMultiplier, 240 * sizeMultiplier],
-      localSizeMultiplier: 1,
-      patrol: [70, 120],
-      speed: [5, 5],
-    },
-    {
-      position: [150, 100],
-      size: [180 * sizeMultiplier, 240 * sizeMultiplier],
-      localSizeMultiplier: 1,
-      patrol: [100, 180],
-      speed: [10, 10],
-    },
-  ],
-}
-
-const ball = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-    radius: 15,
-    graphic: document.getElementById('ball'),
-    dx: 1,
-    dy: 1
-}
-
-function drawField() {
-  ctx.drawImage(field.graphic, 0, 0, width, height);
-}
-
-function drawGoal() {
-  ctx.drawImage(goal.graphic, goal.posX, goal.posY, goal.width, goal.height);
-}
-
-function drawMousePoint() {
-  ctx.beginPath();
-  ctx.arc(mouse.x, mouse.y, 3, 0, Math.PI * 2);
-  ctx.fillStyle = 'orange';
-  ctx.fill();
-}
-
-function moveOpponent() {
-
-}
-
+/*
 function drawBall() {
   ctx.drawImage(ball.graphic, ball.x - ball.radius, ball.y - ball.radius, ball.radius * 2, ball.radius * 2);
-  /*
   ctx.beginPath();
   ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
   ctx.fillStyle = 'white';
   ctx.fill();
-  */
 }
 
 function moveBall() {
-    ball.x += ball.dx;
-    ball.y += ball.dy;
+  ball.x += ball.dx;
+  ball.y += ball.dy;
 }
 
 function bounceBall() {
-    // Detect boundaries
-    if(ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0)
-    {
-        ball.dx *= -1;
-    }
-
-    if(ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0)
-    {
-        ball.dy *= -1;
-    }
+  // Detect boundaries
+  if(ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0)
+  {
+    ball.dx *= -1;
+  }
+  
+  if(ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0)
+  {
+    ball.dy *= -1;
+  }
 }
 
 function holdBall() {
@@ -245,65 +471,67 @@ function holdBall() {
     }
   }
 }
+*/
+
+// Goal
+const goal = new Goal(graphics.goal.graphic, graphics.goal.position, graphics.goal.size, 1);
 
 // Player Character
-const player = new Character(graphics.graphic, mouse.x, mouse.y,graphics.width, graphics.height);
+const player = new Player();
 
 // Opponent Characters
 const opposingPlayers = [];
-for(let i = 0; i < opposingCharacters.players.length; i++)
+for(let i = 0; i < graphics.characters.players.length; i++)
 {
-  opposingPlayers.push(new Character(opposingCharacters.graphic, opposingCharacters.players[i].position[0], opposingCharacters.players[i].position[1], opposingCharacters.players[i].size[0] * opposingCharacters.players[i].localSizeMultiplier, opposingCharacters.players[i].size[1] * opposingCharacters.players[i].localSizeMultiplier));
-  console.log("Opponent Added: " + opposingPlayers[i].posX + " : " + opposingPlayers[i].posY);
+  opposingPlayers.push(new Character(graphics.characters.graphic, graphics.characters.players[i].position[0], graphics.characters.players[i].position[1], graphics.characters.players[i].size[0] * graphics.characters.players[i].localSizeMultiplier * sizeMultiplier, graphics.characters.players[i].size[1] * graphics.characters.players[i].localSizeMultiplier * sizeMultiplier));
+  console.log("Opponent Added: " + opposingPlayers[i].position[0] + " : " + opposingPlayers[i].position[1]);
 }
+
+// Ball
+const ball = new Ball(graphics.ball.graphic, graphics.ball.position, graphics.ball.size, 0);
 
 function Update() {
   // Clears canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(graphics.field.graphic, graphics.field.position[0], graphics.field.position[1], graphics.field.size[0], graphics.field.size[1]);
+  goal.DrawGoal();
   
-  // So dev knows the mouse is on the field
-  drawMousePoint();
-
-  drawField();
-  drawGoal();
-  drawBall();
-
+  ball.DrawBall();
+  
   // Update Player Character position and Draw
-  player.posX = mouse.x - player.width/2;
-  player.posY = mouse.y - player.height;
-  player.DrawCharacter();
+  player.DrawPlayer();
   
   // Iterate through opposing characters, Update their positions and Draw
   for(let i = 0; i < opposingPlayers.length; i++)
   {
-    opposingPlayers[i].posX = opposingCharacters.players[i].position[0];
-    opposingPlayers[i].posY = opposingCharacters.players[i].position[1];
+    opposingPlayers[i].position = [graphics.characters.players[i].position[0], graphics.characters.players[i].position[1]];
     opposingPlayers[i].DrawCharacter();
+    opposingPlayers[i].PatrolCharacter();
   }
 
+  player.CatchBall(ball);
 
-  holdBall();
+  player.HoldBall(ball);
 
-  moveBall();
-  bounceBall();
+  ball.MoveBall();
+
+  ball.Score(goal, player);
   
-
   requestAnimationFrame(Update); // reruns the update for animatability as opposed to running the update from here which would near instantly overflow the call stack.
 }
 
 // Event Listeners
 addEventListener("mousemove", (event) => {
-    
-    mouse.x = event.clientX - (pageWidth - width) / 2;
-    mouse.y = (event.clientY - (pageHeight - height) / 2) - 40; // Not sure what the 40 is about, I suspect it's for the h1 element
+  
+  mouse.x = event.clientX - (pageWidth - canvasWidth) / 2;
+  mouse.y = (event.clientY - (pageHeight - canvasHeight) / 2) - 40; // Not sure what the 40 is about, I suspect it's for the h1 element
+
 });
 
-addEventListener('click', kickBall => {
-    if(graphics.holding)
+addEventListener('click', () => {
+    if(player.holding)
     {
-        ball.dy = -graphics.kickStrength;
-        graphics.kicking = true;
-        setTimeout(() => graphics.kicking = false, 300);
+      player.Kick(ball);
     }
 });
 
